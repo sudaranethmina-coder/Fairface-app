8f46bcac7e9b3acb8c027d84987ad42f572ff1eb
sudaranethmina-coder <sudaranethmina@gmail.com>
2026-05-02 12:30:11 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,56 @@
-# =========================================================
-# streamlit_app
-# =========================================================
 import streamlit as st
 import torch
 from PIL import Image
-from torchvision import transforms
 from MLModel import CNN, eval_transform
 
+st.set_page_config(
+    page_title="FairVision AI",
+    layout="wide"
+)
+
 ages = [
-"0-2","3-9","10-19","20-29","30-39",
-"40-49","50-59","60-69","70+"
+    "0-2","3-9","10-19","20-29","30-39",
+    "40-49","50-59","60-69","70+"
 ]
 
 model = CNN(len(ages))
-model.load_state_dict(torch.load("best_fairface_model.pt"))
+checkpoint = torch.load("best_fairface_model.pt", map_location="cpu")
+model.load_state_dict(checkpoint["model_state_dict"])
 model.eval()
 
 transform = eval_transform
 
-st.title("🧠 FairVision Age Classification")
+st.title("🧠 FairVision Age Classifier")
 
-uploaded_file = st.file_uploader("Upload Face Image")
+uploaded_files = st.file_uploader(
+    "Upload Face Images",
+    type=["jpg", "jpeg", "png"],
+    accept_multiple_files=True
+)
 
-if uploaded_file:
-    img = Image.open(uploaded_file)
-    st.image(img)
+# =========================
+# GRID DISPLAY
+# =========================
+if uploaded_files:
 
-    x = transform(img).unsqueeze(0)
-    outputs = model(x)
-    st.write(
-        outputs
-    )
+    cols = st.columns(3)  # 👉 3 images per row
+
+    for idx, file in enumerate(uploaded_files):
+
+        img = Image.open(file).convert("RGB")
+        x = transform(img).unsqueeze(0)
+
+        with torch.no_grad():
+            outputs = model(x)
+            probs = torch.softmax(outputs, dim=1)
+
+            pred_class = torch.argmax(probs, dim=1).item()
+            confidence = probs[0][pred_class].item()
+            predicted_age = ages[pred_class]
+
+        col = cols[idx % 3]  # rotate columns
+
+        with col:
+            st.image(img, use_container_width=True)
+            st.markdown(f"### 🎯 {predicted_age}")
+            st.caption(f"Confidence: {confidence:.2f}")
